4133f63ef3a775a8b19d699ee465bf735363bcc9
David Werth <werth.david@gmail.com>
2021-01-13 11:35:40 +0100

    #2 Time to change back to black is getting longer and longer

--- MMM-BurnIn.js
+++ MMM-BurnIn.js
@@ -1,33 +1,35 @@
 Module.register("MMM-BurnIn", {
 
-    defaults: {
-        updateInterval: 15
-    },
+	defaults: {
+		updateInterval: 15
+	},
 
-    getStyles: function () {
-        return [];
-    },
+	getStyles: function () {
+		return [];
+	},
 
-    start: function () {
-        setInterval(() => {
-            this.sendSocketNotification("DO_ANTI_BURNIN", this.config);
-        }, this.config.updateInterval * 60 * 1000);
-        setInterval(() => {
-            this.sendSocketNotification("DO_ANTI_BURNIN2", this.config);
-        }, (this.config.updateInterval * 60 * 1000) + 5000);
-    },
+	start: function () {
+		setTimeout(() => {
+			this.startAntiBurnin();
+		}, this.config.updateInterval * 60 * 1000);
+	},
 
-    getDom: function () {
-        return document.createElement("div");
-    },
+	getDom: function () {
+		return document.createElement("div");
+	},
 
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "ANTI_BURNIN") {
-            document.getElementsByTagName("body").item(0).style = "filter: invert(1)";
-            this.updateDom(1000);
-        } else if (notification === "ANTI_BURNIN2") {
-            document.getElementsByTagName("body").item(0).style = "filter: invert(0)";
-            this.updateDom(1000);
-        }
-    }
+	startAntiBurnin: function () {
+		document.getElementsByTagName("body").item(0).style = "filter: invert(1)";
+		this.updateDom(1000);
+		setTimeout(() => {
+			document.getElementsByTagName("body").item(0).style = "filter: invert(0)";
+			this.updateDom(1000);
+		}, 5000);
+		setTimeout(() => {
+			this.startAntiBurnin();
+		}, this.config.updateInterval * 60 * 1000);
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -2,16 +2,10 @@ const NodeHelper = require("node_helper");
 
 
 module.exports = NodeHelper.create({
-    start: function () {
-    },
+	start: function () {
+	},
 
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "DO_ANTI_BURNIN") {
-            this.sendSocketNotification("ANTI_BURNIN");
-            return true;
-        }else  if (notification === "DO_ANTI_BURNIN2") {
-            this.sendSocketNotification("ANTI_BURNIN2");
-            return true;
-        }
-    }
+	socketNotificationReceived: function (notification, payload) {
+		return false;
+	}
 });
